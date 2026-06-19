18eff1035ee68d38a0c657ad9bb4459b1d2f453a
devperry <devmichaelperry@gmail.com>
2026-06-19 10:51:00 -0500

    update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'top1-v6'; // Cada cambio importante, se debe cambiar el numero de version :>
+const CACHE_NAME = 'top1-v7'; // Subimos la versión para limpiar caché viejo
 
-const ASSETS =[
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
@@ -8,21 +8,24 @@ const ASSETS =[
   './js/app.js',
   './js/store.js',
   './js/ui.js',
-  './js/cloud.js', 
+  './js/cloud.js',
+  './icons/icon-192.png', // Añadidos para el modo PWA Offline
+  './icons/icon-512.png',
+  './assets/sfx/winTouchSfxPastel.mp3', // Añadidos para que suenen sin internet en el aula
+  './assets/sfx/multitudAplausos.mp3',
+  './assets/sfx/uiClick.mp3',
+  './assets/sfx/eraseCutted.mp3',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); 
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
 });
-
-
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
@@ -40,25 +43,20 @@ self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
-
 self.addEventListener('fetch', (e) => {
-
   if (e.request.url.includes('firestore.googleapis.com') || e.request.url.includes('identitytoolkit')) {
     return;
   }
-
   e.respondWith(
     fetch(e.request)
-      .then((networkResponse) => {
-   
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(e.request, networkResponse.clone());
-          return networkResponse;
-        });
-      })
-      .catch(() => {
-
-        return caches.match(e.request);
-      })
+    .then((networkResponse) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(e.request, networkResponse.clone());
+        return networkResponse;
+      });
+    })
+    .catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
